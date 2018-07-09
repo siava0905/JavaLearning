b9734e02ab89e62c73d0932ce4daaac618d525ad
siava0905 <40494095+siava0905@users.noreply.github.com>
2018-07-09 15:55:32 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,2 +1,28 @@
 public class TestClass {
-}
+    public static void main(String[] args) {
+        int salary1 = 25000;
+       // int salary2 = 30000;
+        int salary3 = 20000;
+        int salary4 = 10000;
+        int salary5 = 10000;
+       // int salary6 = 15000;
+        int salary7 = 15000;
+        int salary8 = 15000;
+        int experience1 = 4;
+       // int experience2 = 6;
+        int experience3 = 3;
+        int experience4 = 1;
+        int experience5 = 1;
+       // int experience6 = 3;
+        int experience7 = 3;
+        int experience8 = 3;
+       // String n1 = "Imia1";
+       // String n2 = "Name2";
+        String n3 = "Name3";
+        String n4 = "Name4";
+        String n5 = "Name5";
+        String n6 = "Name6";
+       // String n7 = "Name7";
+        String n8 = "Name8";
+       // String position1 = "manager";
+        String position2 = "worker";
